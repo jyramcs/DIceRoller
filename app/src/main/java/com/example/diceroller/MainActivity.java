dfb295a907b08ce742695599aa57d25731f40165
jyram <jyramsharma03@gmail.com>
2019-10-24 21:27:18 +0100

    Level 4: Rename the 'Roll the dice button' to 'I am feeling lucky' and Add anotter button 'Let's play D-ICEBREAKERS' When you click on Roll the dice, the same behaviour as the one in Part I happens When you click on Let's play D-ICEBREAKERS', a random question from this list of questions appears on the screen Important: Separate the code for generating a new number in a new method called roll_the_dice(), use this method from the handlers for the click event for both buttons.

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -12,9 +12,14 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score=0;
+    int number=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,32 +59,31 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    int score = 0;
 
-    public void on_button_click(View view)
-    {
+    public int random_num_gen(){
+        Random r = new Random();
+        number = r.nextInt(6);
+        return number;
+    }
 
+    public void on_button_click(View view) {
         TextView tv = this.findViewById(R.id.numbertextView);
         TextView tv2 = this.findViewById(R.id.inputNumberValue);
         TextView tv3 = this.findViewById(R.id.correcttextView);
         TextView tv4 = this.findViewById(R.id.scoretextView);
 
-        Random r = new Random();
-        int number = r.nextInt(6);
+        random_num_gen();//calls random number generator
+
         tv.setText(Integer.toString(number));
+
         int foundval = Integer.parseInt(tv2.getText().toString());
 
-
-        if (foundval >0 || foundval <7)
-        {
-
-            if (foundval==number)
-            {
+        if (foundval >0 || foundval <7) {
+            if (foundval==number) {
                 tv3.setText("Congratulations!");
                 score = score+1;
             }
-            else
-            {
+            else {
                 tv3.setText("Incorrect!");
             }
         }
@@ -87,6 +91,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         tv4.setText(String.valueOf(score));
+
+    }
+
+    public void d_icebreaker(View view)
+    {
+        TextView tv5 = this.findViewById(R.id.numbertextView);
+
+        ArrayList<String> list = new ArrayList<String>(); //create new array
+        list.add("If you could go anywhere in the world, where would you go?");
+        list.add("If you were stranded on a desert island, what three things would you want to take with you?");
+        list.add("If you could eat only one food for the rest of your life, what would that be?");
+        list.add("If you won a million dollars, what is the first thing you would buy?");
+        list.add("If you could spaned the day with one fictional character, who would it be?");
+        list.add("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+
+        tv5.setText(list.get(random_num_gen()));
 
     }
 
