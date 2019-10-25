e788e45768361a75669ade1369a73fc769f1ecfd
jyram <jyramsharma03@gmail.com>
2019-10-25 18:28:35 +0100

    (bonus)Level 4.5: Add another button 'Add a new D-icebreaker'. Clicking on the button takes you on another screen with a text box where you can input a new ice-breaker rule. There are two nuttons on the screen: cancel and save. Clicking either of them takes you back to the previous screen. Read here how to start a new activity

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -1,19 +1,21 @@
 package com.example.diceroller;
 
-import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
 import java.util.Random;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -108,6 +110,13 @@ public class MainActivity extends AppCompatActivity {
 
         tv5.setText(list.get(random_num_gen()));
 
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        //this method takes you to the new screen (DisplayMessageActivity.java)
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
     }
 
 }
